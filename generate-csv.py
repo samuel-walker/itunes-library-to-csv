4d52e7367a5bfb6ffcbd74ae8ff4f55bea0aa20a
Samuel Walker <31997383+samuel-walker@users.noreply.github.com>
2018-09-30 15:02:28 -0700

    CSV generation working
    Script works to generate a CSV of playlist information from the iTunes library file.

--- generate-csv.py
+++ generate-csv.py
@@ -1,52 +1,67 @@
-# Generate CSV of playlists
-# playlist, track id, song, artist, album
+# Generate CSV of playlists from iTunes libary XML
 
-# Using plistlib
+# Using plistlib to read Apple Proprety List format
 import plistlib
+# Using csv to write csv file
+import csv
 
 # List of system-generated playlists to be ignored
-system_list = ['Library','Downloaded','Music','Downloaded','Movies','Downloaded','TV Shows','Podcasts','Audiobooks']
+system_list = [
+    'Library','Downloaded','Music','Downloaded','Movies','Downloaded',
+    'TV Shows','Podcasts','Audiobooks'
 ]
 
 # List of personal playlists to be ignored
-custom_list = ['Bedwetting Cosmonaut Presents: Down By The Water (Spring Mix 2010)','Indie/Rock Playlist: July (2010)','Top 680 Hip Hop Songs','Ricky Gervais XFM','The Ricky Gervais Show','To Edit']
+custom_list = [
+    'Bedwetting Cosmonaut Presents: Down By The Water (Spring Mix 2010)',
+    'Indie/Rock Playlist: July (2010)','Top 680 Hip Hop Songs',
+    'Ricky Gervais XFM','The Ricky Gervais Show','To Edit'
+]
 
 # Ignore both system and custom lists
 lists = system_list + custom_list
 
-# Tried to make a list of playlist types (i.e., smart) to ignore
-# But had trouble implimenting later
+# List of playlist types (i.e., smart/Genius and Folders) to ignore
 types = ['Smart Info','Genius Track ID','Folder']
 
-# Open iTunes library file
+# Load iTunes library file
 with open('Library.xml', 'rb') as fp:
     pl = plistlib.load(fp)
 
-# Create CSV for writing
-import csv
-
-csv = open('songs.csv', "w")
-
-columnTitleRow = "playlist, name, artist, album\n"
-csv.write(columnTitleRow)
-
-# Iterate over library file to ID playlists and extract tracks, match tracks
-for i in pl['Playlists']:
-    if i['Name'] in lists or 'Smart Info' in i or 'Genius Track ID' in i or 'Folder' in i:
-        print('Ignore')
-    else:
-        # print(i['Name'])
-        playlist = i['Name']
-        # print(i['Playlist Items'])
-        for item in i['Playlist Items']:
-            # print(track['Track ID'])
-            trackID = item['Track ID']
-            # print("Track ID = " + str(trackID))
-            # print(trackID)
-            for track in pl['Tracks']:
-                # print("Track = " + track)
-                if int(track) == trackID:
-                    row = playlist + ',' + pl['Tracks'][str(track)]['Name'] + ',' + pl['Tracks'][str(track)]['Artist'] + ',' + pl['Tracks'][str(track)]['Album'] + '\n'
-                    for i in row:
-                        i = i.strip
-                    csv.write(row)
+# Open CSV for writing
+with open('songs.csv', 'wt', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(('playlist', 'name', 'artist', 'album'))
+    # Iterate over library file to find desired playlists
+    for i in pl['Playlists']:
+        # If playlist is in ignore lists, ignore it
+        if i['Name'] in lists:
+            print('Ignore')
+        elif True:
+            # If playlist is in ignored types, ignore it
+            for type in types:
+                if type in i:
+                    print('Ignore')
+                    ignore = True
+                else:
+                    ignore = False
+            # Otherwise, we want it
+            if ignore == True:
+                continue
+            else:
+                playlist = i['Name']
+                print(playlist)
+                # Iterate over playlist items to find Track IDs
+                for item in i['Playlist Items']:
+                    trackID = item['Track ID']
+                    # Iterate over entire library tracks to find matches
+                    for track in pl['Tracks']:
+                        # If a match is found
+                        if int(track) == trackID:
+                            # Selector
+                            t = pl['Tracks'][str(track)]
+                            print(t['Name'])
+                            # Grab info from track
+                            row = [playlist,t['Name'],t['Artist'],t['Album']]
+                            # Add info to the CSV
+                            writer.writerows([row])
