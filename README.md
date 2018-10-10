0e2bca7d5a734b97028aab0e71fd3668fa3263f7
Samuel Walker <31997383+samuel-walker@users.noreply.github.com>
2018-10-09 21:01:22 -0700

    Script finalized

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
 # itunes-to-gpm
 
-This program reads in an iTunes library (exported as Library.xml) and using [gmusic-playlist](https://github.com/soulfx/gmusic-playlist), creates new playlists in a [Google Play Music](http://www.music.google.com) account to match those in the iTunes library. It simply searches GPM using the tags from iTunes, so no guarantee it will find the correct songs!
+This program reads in an iTunes library (exported as Library.xml, File > Library > Export Library) and writes out CSV files of track name, artist, and album for each playlist.
+
+# Requirements
+
+Python 3.x
+
+# Usage
+
+[generate-csv.py](generate-csv.py) reads Library.xml and writes out the CSV files in the same folder.
+
+To use, add Library.xml to this folder and then run the following command in the command prompt (Windows):
+
+`python generate-csv.py`
+
+# Customization
+
+You can modify two pieces of information to customize the script.
+
+## List of system-generated playlists to be ignored
+
+`system_list` contains a list of iTunes-generated playlists to ignore. You can add or remove as you see fit.
+
+```python
+system_list = [
+    'Library','Downloaded','Music','Downloaded','Movies','Downloaded',
+    'TV Shows','Podcasts','Audiobooks'
+]
+```
+
+## List of personal playlists to be ignored
+
+`custom_list` is empty by default. Add any of your own playlists you wish to ignore here.
+
+```python
+custom_list = []
+```
+
+## List of playlist types to ignore
+
+Finally, `types` contains playlist types I wanted to ignore. Again, edit these as you see fit.
+
+```python
+types = ['Smart Info','Genius Track ID','Folder','Distinguished Kind']
+```
+
+# Use case
+
+I use the CSVs with [gmusic-playlist](https://github.com/soulfx/gmusic-playlist) to create new playlists in a [Google Play Music](http://www.music.google.com) account to match those in the iTunes library. It simply uses the CSV to search GPM using the tags from iTunes, so no guarantee it will find the correct songs!
--- generate-csv.py
+++ generate-csv.py
@@ -1,3 +1,5 @@
+# Written for python 3.7.0
+
 # Generate CSVs of playlists from iTunes libary XML
 
 # Using plistlib to read Apple Proprety List format
@@ -12,57 +14,67 @@ system_list = [
 ]
 
 # List of personal playlists to be ignored
-custom_list = [
-    'Bedwetting Cosmonaut Presents: Down By The Water (Spring Mix 2010)',
-    'Indie/Rock Playlist: July (2010)','Top 680 Hip Hop Songs',
-    'Ricky Gervais XFM','The Ricky Gervais Show','To Edit'
-]
+custom_list = []
 
 # Ignore both system and custom lists
 lists = system_list + custom_list
 
 # List of playlist types (i.e., smart/Genius and Folders) to ignore
-types = ['Smart Info','Genius Track ID','Folder']
+types = ['Smart Info','Genius Track ID','Folder','Distinguished Kind']
 
 # Load iTunes library file
 with open('Library.xml', 'rb') as fp:
     pl = plistlib.load(fp)
 
-
 # Iterate over library file to find desired playlists
 for i in pl['Playlists']:
     # If playlist is in ignore lists, ignore it
+    ignore = False
     if i['Name'] in lists:
-        print('Ignore')
-    elif True:
+        print('Ignore (system): ' + i['Name'])
+    else:
         # If playlist is in ignored types, ignore it
+        # Check if it is
         for type in types:
             if type in i:
-                print('Ignore')
                 ignore = True
-            else:
-                ignore = False
-        # Otherwise, we want it
+        # If it is, ignore it
         if ignore == True:
+            print('Ignore (type): ' + i['Name'])
             continue
+        # Otherwise, we want it
         else:
             playlist = i['Name'].replace('/', '')
-            print(playlist)
-            # Open CSV for writing
-            with open(playlist + '.csv', 'wt', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow(('name', 'artist', 'album'))
-                # Iterate over playlist items to find Track IDs
-                for item in i['Playlist Items']:
-                    trackID = item['Track ID']
-                    # Iterate over entire library tracks to find matches
-                    for track in pl['Tracks']:
-                        # If a match is found
-                        if int(track) == trackID:
-                            # Selector
-                            t = pl['Tracks'][str(track)]
-                            print(t['Name'])
-                            # Grab info from track
-                            row = [t['Name'],t['Artist'],t['Album']]
-                            # Add info to the CSV
-                            writer.writerows([row])
+            print('Writing: '+ playlist)
+        ignore = False
+        # Open CSV for writing
+        with open('playlists/' + playlist + '.csv', 'wt', errors='ignore',
+        newline='') as f:
+            writer = csv.writer(f)
+            # header, uncomment if desired
+            # writer.writerow(('title', 'artist', 'album'))
+            # Iterate over playlist items to find Track IDs
+            for item in i['Playlist Items']:
+                trackID = item['Track ID']
+                # Iterate over entire library tracks to find matches
+                for track in pl['Tracks']:
+                    # If a match is found
+                    if int(track) == trackID:
+                        # Selector
+                        t = pl['Tracks'][str(track)]
+                        # print(t['Name'])
+                        # Grab info from track, ignoring missing data
+                        data = {
+                            "Name": '',
+                            "Artist": '',
+                            "Album": ''
+                        }
+                        # make a for loop
+                        for x in data:
+                            try:
+                                data[x] = t[x]
+                            except KeyError:
+                                data[x] = ""
+                        row = [data["Name"], data["Artist"], data["Album"]]
+                        # Add info to the CSV
+                        writer.writerows([row])
